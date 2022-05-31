4b38d63642c246d26a41dd23e75e7b6021c0778e
dazzlemon <daanill13470@gmail.com>
2022-05-31 17:01:49 +0300

    parseExpr

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,21 +1,21 @@
 import Lexer
 
 -- program ::= { command ';'}
-newtype Program = Program [Command]
+newtype Program = Program [Expr]
 
 -- expr ::= number
-data Expr = Number { str::String }
+data Expr = ExprNumber { str::String }
 --        | string
-          | String { str::String }
+          | ExprString { str::String }
 --        | id
-					| Id { str::String }
+					| ExprId { str::String }
 --        | functionCall
 	-- functionCall ::= id '(' {expr} ')'
 					| FunctionCall { id::Expr, args::[Expr] }
 --        | '_'
-					| WildCard
+					| WildCardExpr
 --        | "NULL"
-					| Null
+					| NullExpr
 --        | namedTupleAcess
 					| NamedTuppleAccess { tupleName::Expr, fieldName::Expr }
 --        | lambdaDef
@@ -35,8 +35,12 @@ data Expr = Number { str::String }
 -- namedTupleField ::= id '=' expr
 -- namedTuple ::= '[' {namedTupleField} ']'
 					| NamedTuple { keyValuePairs::[(Expr, Expr)] }
+					| Empty -- kinda like TokenEOF
 
 data ParserError = ParserError
+--               tokens  -> Either (unexpectedTokenOffset, err) (subAST, rest)
+type Subparser = [Token] -> Either (Int,           ParserError) (Expr, [Token])
+ 
 
 parser :: [Token] -> Either ParserError Program
 parser tokens = parser' tokens []
@@ -49,7 +53,7 @@ parser' tokens commands = case parseCommand of
 
 -- command ::= assignment | functionCall | patternMatching
 parseCommand :: [Token] -> Either ParserError (Expr, [Token])
-parseCommand tokens = Left ParserError
+parseCommand tokens = Left ParserError -- TODO: unimplemented
 	where parsedAssignment = parseAssignment tokens
 	      parsedFunctionCall = parseFunctionCall tokens
 	      parsedPatternMatching = parsePatternMatching tokens
@@ -58,7 +62,7 @@ parseCommand tokens = Left ParserError
 parseAssignment :: [Token] -> Either ParserError (Expr, [Token])
 
 -- functionCall ::= id '(' {expr} ')'
-parseFunctionCall :: [Token] -> Either ParserError (Expr, [Token])
+parseFunctionCall :: Subparser
 
 -- patternMatching ::= 'match' expr '{'
 -- 	{expr '->' expr ';'}
@@ -75,18 +79,55 @@ parsePatternMatching :: [Token] -> Either ParserError (Expr, [Token])
 --        | lambdaDef
 --        | namedTuple
 --        | tuple
-parseExpr :: [Token] -> Either ParserError (Expr, [Token])
+parseExpr :: Subparser
+parseExpr [] = Right (Empty, [])
+parseExpr tokens = case (parseErrors, parseGood) of
+	-- only errors from complex expressions ->
+	-- if error is on first token we might still have singleTokenExpr
+	(_ , []) -> case singleTokenExpr of
+		Just expr | fst furthestError == 1 -> Right (expr, tail tokens)
+		_ -> Left furthestError
+	-- no errors -> furthest good
+	([], _ ) -> Right furthestGood
+	-- some errors, and some good -> just return furthest
+	(_ , _ ) -> furthest
+	      -- cases with subExpressions
+	where subparsers = [ parseFunctionCall
+                     , parseNamedTupleAcess
+                     , parseLambdaDef
+                     , parseNamedTuple
+                     , parseTuple
+                     ]
+	      parseResultsEither = map ($ tokens) subparsers
+	      (parseResultsLeft, parseResultsRight) =
+					partition isLeft parseResultsEither
+	      parseErrors = map (\(Left l) -> l) parseResultsLeft
+	      parseGood   = map (\(Right r) -> r) parseResultsLeft
+	      furthestError = maximumBy (compare `on` fst) parseErrors
+	      furthestGood = minimumBy (compare `on` (length . snd)) parseGood
+	      furthestGoodDist = ((-) `on` length) tokens $ snd furthestGood
+	      furthest = if furthestGoodDist > fst furthestError
+	        then Right furthestGood
+	        else Left furthestError
+	      -- single token cases
+	      singleTokenExpr = case head tokens of
+	        Number number -> Just $ ExprNumber number
+	        StringLiteral string -> Just $ ExprString string
+	        Id id -> Just $ ExprId id
+	        WildCard -> Just WildCardExpr
+	        Null -> Just NullExpr
+	        _ -> Nothing
 
 -- namedTupleAcess ::= id ':' id
-parseNamedTupleAcess :: [Token] -> Either ParserError (Expr, [Token])
+parseNamedTupleAcess :: Subparser
 
 -- lambdaDef ::= '(' {id} ')' 
 -- 	'{' { command ';'} '}'
-parseLambdaDef :: [Token] -> Either ParserError (Expr, [Token])
+parseLambdaDef :: Subparser
 
 -- namedTuple ::= '[' {namedTupleField} ']'
 -- namedTupleAcess ::= id ':' id
-parseNamedTuple :: [Token] -> Either ParserError (Expr, [Token])
+parseNamedTuple :: Subparser
 
 -- tuple ::= '[' {expr} ']'
-parseTuple :: [Token] -> Either ParserError (Expr, [Token])
+parseTuple :: Subparser
