b4e763b5f0824e720b4f1ee99b7aa0b80b3b0d92
dazzlemon <daanill13470@gmail.com>
2022-06-03 17:02:49 +0300

    started evaluator

---
+++ src/Evaluator.hs
@@ -0,0 +1,38 @@
+module Evaluator where
+
+import Parser (Program, Expr)
+
+evaluator :: Program -> IO ()
+
+data Variable = VarNumber { varStr::String }
+              | VarString { varStr::String }
+              -- function will be evaluated every time with new args
+              | VarFunction { argNames::[String], body::Program }
+              | VarTuple { fields::[Variable] }
+              | VarNamedTuple { namedFields::[(String, Variable)] }
+
+evalExpr :: [[(String, Variable)]] -> Expr -> IO ()
+-- evalExpr variableScopes expr =
+	-- ExprNumber str -- VarNumber str
+	-- ExprString str -- VarString str
+	-- ExprId str -- get value from deepest variableScope
+	-- FunctionCall id args
+		-- get value from deepest variableScope
+		-- set args in new variableScope
+		-- call evalExpr for body of the function
+	-- WildCardExpr -- probably depends on use -- TODO: ?
+	-- NullExpr -- TODO: ?
+	-- NamedTuppleAccess tupleName fieldName
+		-- get value from deepest variableScope
+		-- get field
+	-- LambdaDef argNames commandList -- VarFunction
+	-- Assignment lhs rhs
+		-- if it doesn't exist in current variableScope
+			-- then: add it to current variableScope
+			-- else: assign new value
+	-- PatternMatching switch cases defaultCase
+		-- try to match switch against cases, if it doesnt match -> default case
+		-- result = rhs expr of first matching case (or default)
+	-- Tuple values -- VarTuple
+	-- NamedTuple -- VarNamedTuple
+	-- Empty -- TODO: remove
