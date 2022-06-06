734727156a2347902e4635033fad9f6a8cc3282a
dazzlemon <daanill13470@gmail.com>
2022-06-06 17:52:35 +0300

    now assignment works properly inside functions + code cleanup

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -2,7 +2,7 @@
 
 module Evaluator where
 
-import System.Exit (exitFailure)
+import System.Exit (die)
 import Parser
 import Control.Monad (mplus)
 import Control.Monad.State
@@ -10,12 +10,11 @@ import Data.List (findIndex, elemIndex)
 import Data.Maybe (catMaybes, mapMaybe)
 import Control.Monad.Trans.Maybe
 
--- newtype EvaluatorError = EvaluatorError String
-
 evaluator :: Program -> IO ()
-evaluator = evaluator' [[]]
+evaluator p = snd $ evaluator' [[]] p
 
--- type InnerFunction = ([[(String, Variable)]] -> EvaluationResult)
+type VarScopes = [[(String, Variable)]]
+type EvalState = (VarScopes, IO ())
 
 data Variable = VarNumber { varStr::String }
               | VarString { varStr::String }
@@ -30,65 +29,22 @@ data Variable = VarNumber { varStr::String }
               --                    }
               deriving (Show)
 
-evaluator' :: [[(String, Variable)]] -> Program -> IO ()
-evaluator' _ [] = return ()
+evaluator' :: VarScopes -> Program -> EvalState
+evaluator' variableScopes [] = (variableScopes, return ())
 evaluator' variableScopes (first:rest) = case first of
   Assignment (ExprId lhs) rhs -> case evalExpr variableScopes rhs of
-    (Just rhsVar, io) -> do
-      io
-      evalAfterAssignment lhs rhsVar variableScopes rest
-  FunctionCall (ExprId "print") args -> if length args /= 1
-    then expectedArgs "print" 1 $ length args
-    else case head args of
-      ExprString str -> printStr str
-      ExprNumber str -> printStr str
-      ExprId str -> printId str
-      fc@(FunctionCall _ _) -> case evalExpr variableScopes fc of
-        (Just arg, io) -> do
-          io
-          case arg of
-            VarString str -> printStr str
-            VarNumber str -> printStr str
-        (Nothing, io) -> do
-          io
-          exitWithErrorMessage "error: argument expression wasn't evaluated"
-      _ -> exitWithErrorMessage "not implemented yet" -- TODO:
-  FunctionCall (ExprId fname) args -> case findVarById fname variableScopes of
-    Just (VarFunction argNames body) -> if length argNames /= length args
-      then expectedArgs fname (length argNames) (length args)
-      else do
-        let evalExprs = map (evalExpr variableScopes) args
-        let (argMaybeVars, ios) = unzip evalExprs
-        let argVars = catMaybes argMaybeVars
-        sequence_ ios
-        evaluator' (zip argNames argVars:variableScopes) body
-        evaluator' variableScopes rest
-    Just _ -> exitWithErrorMessage $ "error: `" ++ fname ++ "` is not a function"
-    Nothing -> exitWithErrorMessage $ "error: `" ++ fname ++ "` is not initialized"
-  PatternMatching switch cases defaultCase -> do
-    let (lefts, rights) = unzip cases
-    -- TODO: if lhs is a function it will have to be evaluated, so one more io
-    let (io, maybeIndex) = findMatch variableScopes switch lefts
-    let expr = case maybeIndex of
-                Just i -> rights !! i
-                Nothing -> defaultCase
-    io
-    evaluator' variableScopes [expr]
-    evaluator' variableScopes rest
-  NullExpr -> evaluator' variableScopes rest -- just don't print it
-  other -> exitWithErrorMessage $ "not implemented yet: " ++ show other -- TODO:
-  where printStr str = do
-          putStrLn str
-          evaluator' variableScopes rest
-        printId str = case findVarById str variableScopes of
-          Just var -> case var of
-            VarString val -> printStr val
-            VarNumber val -> printStr val
-            _ -> exitWithErrorMessage "not implemented yet" -- TODO:
-          _ -> exitWithErrorMessage $ "error: variable `" ++ str ++ "` not assigned, variableScopes: " ++ show variableScopes -- TODO:
+    (Just rhsVar, io) ->
+      let variableScopes' = variableScopesWith lhs rhsVar variableScopes
+      in evaluator' variableScopes' rest
+  FunctionCall {} -> case evalExpr variableScopes first of
+    (_, io) -> (variableScopes, io)
+  PatternMatching {} -> case evalExpr variableScopes first of
+    (_, io) -> (variableScopes, io)
+  NullExpr -> evaluator' variableScopes rest -- just skip it
+  other -> (variableScopes, die $ "error: invalid comand: " ++ show other)
 
 expectedArgs :: String -> Int -> Int -> IO ()
-expectedArgs fname expected got = exitWithErrorMessage $ "error: `" ++ fname
+expectedArgs fname expected got = die $ "error: `" ++ fname
   ++ "` expected " ++ show expected ++ " argument(s), but got "
   ++ show got
 
@@ -98,25 +54,17 @@ findMatch = findMatch' 0
 findMatch' :: Int -> [[(String, Variable)]] -> Expr -> [Expr] -> (IO (), Maybe Int)
 findMatch' _ _ _ [] = (return (), Nothing)
 findMatch' i variableScopes lhs (rhs:rest) = (io, maybeVar)
-  where io = do 
-              io1
-              io2
-              io3
+  where io = io1 >> io2 >> io3
         (mbLhsVar, io1) = evalExpr variableScopes lhs
         (mbRhsVar, io2) = evalExpr variableScopes rhs
         (io3, maybeVar) = case (mbLhsVar, mbRhsVar) of
-          (Nothing, _) -> ( exitWithErrorMessage "error: lhs didn't evaluate"
+          (Nothing, _) -> ( die "error: lhs didn't evaluate"
                           , Nothing)
-          (_, Nothing) -> ( exitWithErrorMessage "error: rhs didn't evaluate"
+          (_, Nothing) -> ( die "error: rhs didn't evaluate"
                           , Nothing)
           (Just lhs', Just rhs') -> if match lhs' rhs'
             then (return (), Just i)
             else findMatch' (i + 1) variableScopes lhs rest
-
-exitWithErrorMessage :: String -> IO ()
-exitWithErrorMessage err = do
-  putStrLn err
-  exitFailure 
 
 match :: Variable -> Variable -> Bool
 match (VarNumber lhs) (VarNumber rhs) = rhs == lhs
@@ -133,17 +81,25 @@ findVarById name variableScopes = do
   return (snd $ variableScopes !! i !! j)
 
 containsVar :: String -> [(String, Variable)] -> Bool
-containsVar name = any $ (== name) . fst
+containsVar name = any ((== name) . fst)
 
-evalAfterAssignment :: String -> Variable -> [[(String, Variable)]] -> Program -> IO ()
-evalAfterAssignment lhs rhs variableScopes commands = do
-  -- putStrLn $ "variableScopes: " ++ show variableScopes'
-  evaluator' variableScopes' commands
-  where variableScopes' = case maybeI of
-          Just i -> case findIndex ((== lhs) . fst) (variableScopes !! i) of
-            Just j -> replaceNth i (replaceNth j (lhs, rhs) (variableScopes !! i)) variableScopes
-          Nothing -> ((lhs, rhs):head variableScopes):tail variableScopes
-        maybeI = findIndex (containsVar lhs) variableScopes
+variableScopesWith :: String -> Variable -> VarScopes -> VarScopes
+variableScopesWith lhs rhs variableScopes = case maybeVariableScopes of
+  Just r -> r
+  Nothing -> (newKV:head variableScopes):tail variableScopes
+  where newKV = (lhs, rhs)
+        maybeVariableScopes = do
+          -- find first (deepest possible) variableScope
+          -- that contains var to be assigned to
+          i <- findIndex (containsVar lhs) variableScopes
+          let ith = variableScopes !! i
+          -- if there is a scope that has var assigned to
+          -- find index of the var in the scope
+          j <- findIndex ((== lhs) . fst) ith
+          -- replace j-th variable value with rhs
+          let ith' = replaceNth j newKV ith
+          -- replace i-th scope with modified one:
+          return $ replaceNth i ith' variableScopes
         
 replaceNth :: Int -> a -> [a] -> [a]
 replaceNth _ _ [] = []
@@ -159,13 +115,12 @@ evalExpr _ (LambdaDef argNames commandList) = (Just $ VarFunction argNames' comm
   where argNames' = map str argNames
 evalExpr variableScopes (FunctionCall (ExprId fname) args) = 
   case findVarById fname variableScopes of
-    Just (VarFunction argNames body) -> case evalExpr (variableScopesWith argNames) (last body) of
-      (result, io) -> (result, do
-        -- putStrLn "functionCall evaluation"
-        callVarFunc (init body) argNames
-        io
-        )
-    Just _ -> (Nothing, exitWithErrorMessage $ "error: `" ++ fname ++ "` is not a function")
+    Just (VarFunction argNames body) -> if length argNames /= length args
+      then (Nothing, expectedArgs fname (length argNames) (length args))
+      else case evaluator' (variableScopesWith argNames) (init body) of
+        (varScopes', io1) -> case evalExpr varScopes' (last body) of
+          (res, io2) -> (res, io1 >> io2)
+    Just _ -> (Nothing, die $ "error: `" ++ fname ++ "` is not a function")
     Nothing -> case fname of
       "print" -> if length args /= 1
         then (Nothing, expectedArgs "print" 1 $ length args)
@@ -174,30 +129,21 @@ evalExpr variableScopes (FunctionCall (ExprId fname) args) =
           ExprNumber str -> (Nothing, putStrLn str)
           ExprId str -> (Nothing, putStrLn str)
           fc@(FunctionCall _ _) -> case evalExpr variableScopes fc of
-            (Just arg, io) -> (Nothing, do
-              io
+            (Just arg, io) -> (Nothing, io >>
               case arg of
                 VarString str -> putStrLn str
                 VarNumber str -> putStrLn str
               )
-            (Nothing, io) -> (Nothing, do
-              io
-              exitWithErrorMessage "error: argument expression wasn't evaluated"
-              )
-          _ -> (Nothing, exitWithErrorMessage "not implemented yet") -- TODO:
+            (Nothing, io) -> (Nothing, io)
+            -- (Nothing, io) -> (Nothing, io >> die "error: argument expression wasn't evaluated")
+          _ -> (Nothing, die "not implemented yet") -- TODO:
       _ | fname `elem` ["add", "sub"] -> binaryNumFun
-      _ -> (Nothing, exitWithErrorMessage $ "error: `" ++ fname ++ "` is not initialized")
-  where callVarFunc body argNames = if length argNames /= length args
-          then expectedArgs fname (length argNames) (length args)
-          else do
-            sequence_ ios
-            evaluator' (variableScopesWith argNames) body
-        mbVar = evalExpr variableScopes 
+      _ -> (Nothing, die $ "error: `" ++ fname ++ "` is not initialized, varScopes: " ++ show variableScopes)
+  where mbVar = evalExpr variableScopes 
         argVars = catMaybes argMaybeVars
         (argMaybeVars, ios) = unzip evalExprs
         evalExprs = map (evalExpr variableScopes) args
-        variableScopesWith argNames =
-          zip argNames argVars:variableScopes
+        variableScopesWith argNames = zip argNames argVars:variableScopes
         binaryNumFun = if argCount /= 2
             then err
             else mergeTwoExprs f1 f2 variableScopes a1 a2
@@ -210,20 +156,14 @@ evalExpr variableScopes (FunctionCall (ExprId fname) args) =
                   "sub" -> ((-), (-))
 
 evalExpr variableScopes (PatternMatching switch cases defaultCase) = case evalExpr variableScopes expr of
-  (res, io2) -> (res, do
-    -- putStrLn "patternMatching evaluation"
-    io
-    io2
-    )
+  (res, io2) -> (res, io >> io2)
   where (lefts, rights) = unzip cases
         (io, maybeIndex) = findMatch variableScopes switch lefts
         expr = case maybeIndex of
           Just i -> rights !! i
           Nothing -> defaultCase
-evalExpr _ e = (Nothing, exitWithErrorMessage $ "error: this expression can't be evaluated: " ++ show e)
-
-type VarScopes = [[(String, Variable)]]
-type EvalState = (VarScopes, IO ())
+evalExpr _ NullExpr = (Just VarNull, return ()) 
+evalExpr _ e = (Nothing, die $ "error: this expression can't be evaluated: " ++ show e)
 
 type BinaryFun a = (a -> a -> a)
 
@@ -252,12 +192,13 @@ evalNum expr = MaybeT $ do
   (varScopes, io) <- get
   case evalExpr varScopes expr of
     (res, io') -> do
-      put (varScopes, mappend io io')
+      put (varScopes, io >> io')
       case res of
         Just (VarNumber numStr) -> return (Just numStr)
         Nothing -> err "error: can't evaluate expr"
         _ -> err "expected number argument"
     where err msg = do
             (varScopes, io) <- get
-            put (varScopes, mappend io $ exitWithErrorMessage msg)
+            -- put (varScopes, io >> die msg)
+            put (varScopes, io)
             return Nothing
